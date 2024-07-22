3c6b8aad4bc5a2d74518d1e22ab7b39fdb52b227
KS <kyusang.dev@gmail.com>
2024-07-22 11:18:51 +0900

    Firebase 연동확인 코드

--- NoteApp/NoteApp/NoteAppApp.swift
+++ NoteApp/NoteApp/NoteAppApp.swift
@@ -12,6 +12,16 @@ class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         FirebaseApp.configure()
+        
+        // Firebase 연동 확인 코드
+        /*
+         if FirebaseApp.app() != nil {
+            print("Firebase is configured and ready to use")
+        } else {
+            print("Firebase configuration failed")
+        }
+         */
+        
         return true
     }
 }
