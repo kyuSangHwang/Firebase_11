b1a2a4f8f3958f1d3e709d1aab4c7b24c37bb246
KS <kyusang.dev@gmail.com>
2024-07-22 14:18:22 +0900

    detailView, navigationLink 추가

--- NoteApp/NoteApp/ContentView.swift
+++ NoteApp/NoteApp/ContentView.swift
@@ -17,9 +17,11 @@ struct ContentView: View {
         NavigationStack {
             List {
                 ForEach(viewModel.notes, id: \.id) { note in
-                    VStack(alignment: .leading) {
-                        Text(note.title ?? "")
-                            .font(.system(size: 22, weight: .regular))
+                    NavigationLink(destination: DetailsView(note: note)) {
+                        VStack(alignment: .leading) {
+                            Text(note.title ?? "")
+                                .font(.system(size: 22, weight: .regular))
+                        }
                     }
                 }
             }
@@ -29,7 +31,7 @@ struct ContentView: View {
             .navigationTitle("Notes")
             .toolbar {
                 ToolbarItemGroup(placement: .bottomBar) {
-                    Text(" X notes")
+                    Text(" \(viewModel.notes.count) notes")
                     Spacer()
                     Button {
                         showSheet.toggle()
