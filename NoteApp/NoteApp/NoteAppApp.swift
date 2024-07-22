93590e7945a32842684b96ace752cb454d69ec73
KS <kyusang.dev@gmail.com>
2024-07-22 10:13:07 +0900

    Initialize NoteApp Project

---
+++ NoteApp/NoteApp/NoteAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteAppApp.swift
+//  NoteApp
+//
+//  Created by 황규상 on 7/22/24.
+//
+
+import SwiftUI
+
+@main
+struct NoteAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
