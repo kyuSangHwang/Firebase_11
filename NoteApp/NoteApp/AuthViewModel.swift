9853afaf386021468f924f5e956c20f37ca98fcc
KS <kyusang.dev@gmail.com>
2024-07-23 14:08:04 +0900

    AuthViewModel, HolderView 추가

---
+++ NoteApp/NoteApp/AuthViewModel.swift
@@ -0,0 +1,19 @@
+//
+//  AuthViewModel.swift
+//  NoteApp
+//
+//  Created by 황규상 on 7/23/24.
+//
+
+import SwiftUI
+import FirebaseAuth
+
+final class AuthViewModel: ObservableObject {
+    @Published var user: User?
+    
+    func listenToAuthState() {
+        Auth.auth().addStateDidChangeListener { [weak self] _, user in
+            self?.user = user
+        }
+    }
+}
