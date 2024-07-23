a5525534203367d5ae1ae5b89bbab61cd08b57d2
KS <kyusang.dev@gmail.com>
2024-07-23 14:10:41 +0900

    SignUpView 추가

---
+++ NoteApp/NoteApp/ResetPasswordView.swift
@@ -0,0 +1,18 @@
+//
+//  ResetPasswordView.swift
+//  NoteApp
+//
+//  Created by 황규상 on 7/23/24.
+//
+
+import SwiftUI
+
+struct ResetPasswordView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    ResetPasswordView()
+}
